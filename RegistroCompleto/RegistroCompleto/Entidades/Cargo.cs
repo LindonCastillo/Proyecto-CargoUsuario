1bb3f14993fe756991bf6b2e3b8059efcd72a4ac
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-05-21 16:35:57 -0400

    Hciendo el registro Cargos

---
+++ RegistroCompleto/RegistroCompleto/Entidades/Cargo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace RegistroCompleto.Entidades
+{
+
+
+    public class Cargo
+    {
+        [Key]
+        int CargoId { get; set; }
+        string Descripcion { get; set; }
+
+        public Cargo()
+        {
+            CargoId = 0;
+            Descripcion = string.Empty;
+        }
+    }
+
+
+}
